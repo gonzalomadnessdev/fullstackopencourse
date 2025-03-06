7af2943872c150115cb40408ea782e11319d6598
Gonzalo Albornoz <galbornoz@sgfintech.com.ar>
2025-03-06 20:52:30 -0300

    refactor App arrow function

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Statistics from './Statistics'
 
-function App() {
+const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
