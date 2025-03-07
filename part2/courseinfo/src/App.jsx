4523d141c0398c78cb0db1e63daf4d49ff6a8fa8
Gonzalo Albornoz <galbornoz@sgfintech.com.ar>
2025-03-06 23:07:24 -0300

    part2, exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,40 @@
+import Header from "./Header"
+import Content from "./Content"
+import Total from "./Total"
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      // {
+      //   id: 3,
+      //   name: 'State of a component',
+      //   exercises: 14,
+      // }
+    ],
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={
+          course.parts.map((p) => p.exercises).reduce((acc, curr) => acc + curr, 0)
+        }
+      />
+    </div>
+  )
+}
+
+export default App
