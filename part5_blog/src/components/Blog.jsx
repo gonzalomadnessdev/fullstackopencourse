8dfa80d6dc8da1ab846a083a52f72a6f7ae4a037
Gonzalo Albornoz <galbornoz@sgfintech.com.ar>
2025-03-25 16:23:10 -0300

    part5, 5.7

--- part5_blog/src/components/Blog.jsx
+++ part5_blog/src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}, {blog.author} 
+        <button type="button" onClick={()=> setShow(!show)}>{(show) ? 'hide': 'view'}</button>
+        {show && <>
+          <br /> likes: {blog.likes}
+          <br /> {blog.url}
+          <br /> {blog.user.name}
+        </>}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
